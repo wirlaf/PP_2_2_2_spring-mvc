60070c7667d8d55b675f6b3c2e3d7a0658837be7
lifanovdaniil <lifanovdaniil@gmail.com>
2024-08-20 13:26:45 +0400

    готово к проверке

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -1,11 +1,9 @@
 package web.dao;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import web.model.User;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
